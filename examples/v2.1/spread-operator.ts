7c71e2283fd72e9542cac1d6d9c49ccc53cd42e3
Pablo Núñez <pablonete@gmail.com>
2016-12-08 03:09:36 +0100

    Updated to released TS2.1. spread & rest operators.

--- examples/v2.1/spread-operator.ts
+++ examples/v2.1/spread-operator.ts
@@ -1,11 +1,33 @@
-// Cannot get this to work, even with target: "es2017"
-
+// Syntax replacement for object.assign() or $.extend()
 function spreadProperties() {
-    var obj = { x: 1, y: 2 };
-    var obj1 = { ...obj, z: 3, y: 4 }; 
+    const xy = { x: 1, y: 2 };
+    const yz = { y: 3, z: 4 }; 
+
+    const copy = { ...xy };
+
+    const merged = { ...xy, ...yz };
+    // Last wins, so merged.y === 3
+
+    // How to use default props
+    const defaultToZero = {
+        x: 0,
+        y: 0,
+        z: 0,
+        ...xy,
+    }
 }
 
+// Extends the destructuring operator
 function restProperties() {
-    var obj = {x:1, y: 1, z: 1};
-    var {z, ...obj1} = obj;
+    const xyz = { x: 1, y: 1, z: 1 };
+
+    const { z, ...xy } = xyz;
+}
+
+function restInArrayArgument([ head, ...tail ]: number[]) {
+
+}
+
+function restInObjectArgument({ name, ...error }: Notification) {
+
 }
